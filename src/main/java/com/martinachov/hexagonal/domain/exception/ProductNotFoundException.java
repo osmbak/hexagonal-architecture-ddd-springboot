abb34083ba236fad90bb10df92319337a216d1cd
Your Name <oel@gmail.c>
2025-12-23 23:05:34 +0100

    comment exception branche_chicky1

--- src/main/java/com/martinachov/hexagonal/domain/exception/ProductNotFoundException.java
+++ src/main/java/com/martinachov/hexagonal/domain/exception/ProductNotFoundException.java
@@ -1,5 +1,5 @@
 package com.martinachov.hexagonal.domain.exception;
-//exception branche_chicky
+//exception branche_chicky 1
 public class ProductNotFoundException extends RuntimeException{
     
     public ProductNotFoundException(String msg){
