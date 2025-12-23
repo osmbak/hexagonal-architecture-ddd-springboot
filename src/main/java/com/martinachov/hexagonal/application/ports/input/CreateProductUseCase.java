014e8c7e2006237455cf49701a4e690ab19ed6dc
Your Name <oel@gmail.c>
2025-12-23 10:24:24 +0100

    comment interface ports

--- src/main/java/com/martinachov/hexagonal/application/ports/input/CreateProductUseCase.java
+++ src/main/java/com/martinachov/hexagonal/application/ports/input/CreateProductUseCase.java
@@ -2,6 +2,7 @@ package com.martinachov.hexagonal.application.ports.input;
 
 import com.martinachov.hexagonal.domain.model.Product;
 
+//all method Repo
 public interface CreateProductUseCase {
    
     Product createProduct(Product product);
